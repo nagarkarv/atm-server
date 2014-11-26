4c335b3b7144bf187bfc199c5522bf0cb46287d6
Vikram Nagarkar <vikram.nagarkar@yahoo.com>
2014-11-26 15:34:20 +0000

    Initial ATM Server
    Initial ATM Server

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+		console.log('Request to connect from server:' + socket);
+		socket.on('atm:signup', function(userInfo){
+			console.log('----Sign up initiated for: '+userInfo.userName + '----');
+			console.log('First Name: '+userInfo.firstName);
+			console.log('Last Name: '+userInfo.lastName);
+			console.log('EMail Name: '+userInfo.email);
+			console.log('----------------------------');
+		});
+		socket.on('atm:login', function(userInfo){
+			console.log('Login initiated for: '+userInfo.userName);
+		});
+		socket.on('atm:logout', function(userInfo){
+			console.log('User Logout: '+userInfo.userName);
+		});
+		
+	});
+
+server.listen(8000,function(){
+	console.log('Server listening on port 8000');
+	console.log('http://127.0.0.1:8000/');
+});
